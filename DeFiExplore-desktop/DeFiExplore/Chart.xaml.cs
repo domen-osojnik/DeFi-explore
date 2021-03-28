0a87d79f21ac9b0f3304e2e8758e86c5e3ba9e3e
Prgan <prga99@gmail.com>
2021-03-28 22:57:49 +0200

    New desktop version added.

---
+++ DeFiExplore-desktop/DeFiExplore/Chart.xaml.cs
@@ -0,0 +1,78 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeFiExplore
+{
+    /// <summary>
+    /// Interaction logic for Chart.xaml
+    /// </summary>
+    public partial class Chart : UserControl, INotifyPropertyChanged
+    {
+        private string[] _labels;
+        public Chart()
+        {
+            InitializeComponent();
+            var listKlines = Kline.getKlines();
+            
+            ChartValues<OhlcPoint> data = new ChartValues<OhlcPoint>();
+            string[] array = new string[listKlines.Count];
+            int counter = 0;
+            foreach (var point in listKlines)
+            {
+                
+                data.Add(new OhlcPoint(point.Open, point.High, point.Low, point.Close));
+                array[counter] = point.OpenTime.ToShortDateString();
+                counter++;
+            }
+            
+            
+            
+            SeriesCollection = new SeriesCollection
+            {
+                new CandleSeries
+                {
+                    Values = data
+                   
+                }
+            };
+            Labels = array;
+           
+            DataContext = this;
+        }
+        public SeriesCollection SeriesCollection { get; set; }
+
+        public string[] Labels
+        {
+            get { return _labels; }
+            set
+            {
+                _labels = value;
+                OnPropertyChanged("Labels");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null) PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+    }
+}
---
+++ DeFiExplore-desktop/DeFiExplore/Kline.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeFiExplore
+{
+    public class Kline
+    {
+        //public string symbol { get; set; }
+        public DateTime OpenTime { get; set; }
+        public double Open { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Close { get; set; }
+        public double Volume { get; set; }
+        public Kline(string line)
+        {
+            var split = line.Split(';');
+            OpenTime = Convert.ToDateTime(split[0]);
+            Open = Convert.ToDouble(split[1]);
+            High = Convert.ToDouble(split[2]);
+            Low = Convert.ToDouble(split[3]);
+            Close = Convert.ToDouble(split[4]);
+            Volume = Convert.ToDouble(split[5]);
+        }
+        public static List<Kline> getKlines()
+        {
+            var data = File.ReadLines(@"historicBTCdaily.csv").Select(line => new Kline(line)).ToList();
+            return data;
+        }
+    }
+}
